868bcd092688ed4f4c8fa74aad0e99cef0945b6f
MDeMaria <Mike.A.DeMaria@gmail.com>
2018-05-02 18:19:26 -0400

    kaldi thing

---
+++ test_interface.py
@@ -0,0 +1,14 @@
+import subprocess
+import os
+orig_dir = os.getcwd()
+path_to_decoder = "/pykaldi/tools/kaldi/egs/aspire/s5"
+path_to_wav_file = "/pykaldi/tools/kaldi/egs/aspire/s5/short_sample.wav"
+os.chdir(path_to_decoder)
+decode_commands = "sh decode_test2.sh " + path_to_wav_file
+dirty_transcript = subprocess.check_output([decode_commands],cwd=path_to_decoder, shell=True, stderr=subprocess.STDOUT)#,check=True)
+transcript = []
+for line in dirty_transcript.decode('UTF-8').split('\n'):
+    if line[:13]=='utterance-id1':
+        transcript.append(line[13:])
+#go back to original path
+os.chdir(orig_dir)
