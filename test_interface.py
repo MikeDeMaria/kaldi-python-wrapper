410bbec779f5afadebba8b05db141db4cdf9f4c4
MDeMaria <Mike.A.DeMaria@gmail.com>
2018-05-02 21:54:03 -0400

    kaldi thingy3

--- test_interface.py
+++ test_interface.py
@@ -1,14 +1,29 @@
 import subprocess
 import os
-orig_dir = os.getcwd()
-path_to_decoder = "/usr/local/kaldi/egs/aspire/s5"
-path_to_wav_file = "/usr/local/kaldi/egs/aspire/s5/short_sample.wav"
-os.chdir(path_to_decoder)
-decode_commands = "sh decode_test.sh " + path_to_wav_file
-dirty_transcript = subprocess.check_output([decode_commands],cwd=path_to_decoder, shell=True, stderr=subprocess.STDOUT)#,check=True)
-transcript = []
-for line in dirty_transcript.decode('UTF-8').split('\n'):
-    if line[:13]=='utterance-id1':
-        transcript.append(line[13:])
-#go back to original path
-os.chdir(orig_dir)
+from os import listdir
+from os.path import isfile, join
+
+def speechToText(file):
+	orig_dir = os.getcwd()
+
+	path_to_decoder = "/usr/local/kaldi/egs/aspire/s5"
+	path_to_audio   = "/usr/local/audio_files"
+
+	os.chdir(path_to_decoder)
+
+	decode_commands = "sh decode_test.sh " + path_to_audio
+
+	dirty_transcript = subprocess.check_output([decode_commands],cwd = path_to_decoder, shell = True, stderr = subprocess.STDOUT)
+
+	transcript = ""
+
+	for line in dirty_transcript.decode('UTF-8').split('\n'):
+		if line[:13] == 'utterance-id1':
+			transcript = line[13:]
+			textfile = open(path_to_audio + "/" + os.path.splitext(os.path.basename(file))[0] + ".txt", 'w')
+			break
+
+onlyfiles = [f for f in listdir(path_to_audio) if isfile(join(path_to_audio, f))]
+
+for i in onlyfiles:
+    speechToText(i)
